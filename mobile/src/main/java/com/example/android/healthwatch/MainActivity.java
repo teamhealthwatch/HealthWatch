60f5aae8cd4bafff4db2ddbef630af25a5b8401f
Buck417 <buck417@gmail.com>
2017-04-03 17:35:42 -0600

    added login page

--- mobile/src/main/java/com/example/android/healthwatch/MainActivity.java
+++ mobile/src/main/java/com/example/android/healthwatch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.healthwatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login(View view){
+       startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+
 }
