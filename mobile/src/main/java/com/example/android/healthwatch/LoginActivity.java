167dba3a5370bed2e51c073467ff34e2460364dc
Buck417 <buck417@gmail.com>
2017-04-13 14:48:15 -0600

    Got registration talking to server

--- mobile/src/main/java/com/example/android/healthwatch/LoginActivity.java
+++ mobile/src/main/java/com/example/android/healthwatch/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.healthwatch;
 
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
@@ -28,9 +29,24 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
