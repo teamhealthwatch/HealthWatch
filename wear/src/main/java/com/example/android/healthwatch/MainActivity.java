8f8c205cd90d367a04a0c7b15d83a9193fe8a042
taneyyy <taneyyy@gmail.com>
2017-04-13 15:27:19 -0600

    Watch: working on sensor

--- wear/src/main/java/com/example/android/healthwatch/MainActivity.java
+++ wear/src/main/java/com/example/android/healthwatch/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.android.healthwatch;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
-    private TextView mTextView;
+//    private TextView mTextView;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView heartRateView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        // For testing
+
+
+        setContentView(heartRateView);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+//                mTextView = (TextView) stub.findViewById(R.id.text);
+
             }
         });
     }
 
+    private void measureHeartRate() {
 
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 }
