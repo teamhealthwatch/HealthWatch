2fa3301bef0dbaf0b82b723ac7f2734f877559e6
Buck417 <buck417@gmail.com>
2017-04-24 17:10:33 -0600

    nothing to update really

--- wear/src/main/java/com/example/android/healthwatch/MainActivity.java
+++ wear/src/main/java/com/example/android/healthwatch/MainActivity.java
@@ -8,6 +8,8 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
