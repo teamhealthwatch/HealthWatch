e8c0a8317f55ef8c3458d7e3c76bc0540cbe1d21
Buck417 <buck417@gmail.com>
2017-11-16 22:35:40 -0700

    Added account activity

---
+++ mobile/src/main/java/com/example/android/healthwatch/Activities/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.healthwatch.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.healthwatch.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
