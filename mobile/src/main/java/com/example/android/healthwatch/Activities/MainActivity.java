28e7adc7cfdf5cb2ec6662a84f5c95cef6805af3
Ryan Fletcher <buck417@gmail.com>
2017-11-28 17:51:17 -0700

    Added Permission Requests from user

--- mobile/src/main/java/com/example/android/healthwatch/Activities/MainActivity.java
+++ mobile/src/main/java/com/example/android/healthwatch/Activities/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.android.healthwatch.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
 import com.example.android.healthwatch.R;
 import com.google.firebase.auth.FirebaseAuth;
+
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.SEND_SMS;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -18,6 +29,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button buttonSignIn;
     Button buttonSignUp;
     Button skip;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
+
 
 
 
@@ -41,6 +56,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         skip = (Button) findViewById(R.id.skip);
         skip.setOnClickListener(this);
+
+        if(!checkPermission()){
+            requestPermission();
+        }
+
     }
 
     @Override
@@ -74,6 +94,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return email;
         }
     }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), SEND_SMS);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{CALL_PHONE, SEND_SMS}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean phoneAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean smsAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (phoneAccepted && smsAccepted) {
+                        Log.i("Permissions","Permissions Accepted");
+                    }
+
+                    else {
+
+                        Log.i("Permissions","Permissions Denied");
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CALL_PHONE)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CALL_PHONE, SEND_SMS},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
