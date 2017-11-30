ae2936dc6a1431bc516d87d97c7ca209064dc52e
taneyyy <taneyyy@gmail.com>
2017-11-30 12:04:31 -0700

    Mobile: quick commit before merging. Created HeartRateService on Mobile.

---
+++ mobile/src/main/java/com/example/android/healthwatch/HeartRateService.java
@@ -0,0 +1,245 @@
+package com.example.android.healthwatch;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.example.android.healthwatch.Activities.MedConditionActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Yan Tan on 11/29/2017.
+ */
+
+public class HeartRateService extends Service implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private final String HEART_RATE = "/heart_rate";
+    private GoogleApiClient googleApiClient;
+    private NodeApi.NodeListener nodeListener;
+    private String remoteNodeId;
+    private MessageApi.MessageListener messageListener;
+
+    private String TAG = "HeartRateService mobile";
+    NotificationManager mNotificationManager;
+
+    int numMessages = 0;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.v(TAG, "onStartCommand");
+
+
+        // Create NodeListener that enables buttons when a node is connected and disables buttons when a node is disconnected
+        nodeListener = new NodeApi.NodeListener() {
+            @Override
+            public void onPeerConnected(Node node) {
+                remoteNodeId = node.getId();
+            }
+
+            @Override
+            public void onPeerDisconnected(Node node) {
+
+            }
+        };
+
+        // Create MessageListener that receives messages sent from a wearable
+        messageListener = new MessageApi.MessageListener() {
+            @Override
+            public void onMessageReceived(MessageEvent messageEvent) {
+
+                String payload = null;
+                if (messageEvent.getPath().equals(HEART_RATE)) {
+                    try {
+                        payload = new String(messageEvent.getData(), "utf-8");
+                    }
+                    catch(UnsupportedEncodingException e){
+                        Log.i("Exception", "thrown encoding");
+                    }
+
+                    // TODO: send heart rate to HomePageActivity
+//                    heartRate.setText(payload);
+                    if(payload != null) {
+                        //setProgressBar(Integer.parseInt(payload));
+                        int hR = (Integer.parseInt(payload));
+                        makePhoneCall(hR);
+                    }
+                }
+                else{
+                    Log.i("heart rate info", "couldn't get in");
+                }
+            }
+        };
+
+
+        // Create GoogleApiClient
+        googleApiClient = new GoogleApiClient.Builder(getApplicationContext()).addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle bundle) {
+                // Register Node and Message listeners
+                Wearable.NodeApi.addListener(googleApiClient, nodeListener);
+                Wearable.MessageApi.addListener(googleApiClient, messageListener);
+                // If there is a connected node, get it's id that is used when sending messages
+                Wearable.NodeApi.getConnectedNodes(googleApiClient).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+                    @Override
+                    public void onResult(NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
+                        if (getConnectedNodesResult.getStatus().isSuccess() && getConnectedNodesResult.getNodes().size() > 0) {
+                            remoteNodeId = getConnectedNodesResult.getNodes().get(0).getId();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+            }
+        }).addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(ConnectionResult connectionResult) {
+                if (connectionResult.getErrorCode() == ConnectionResult.API_UNAVAILABLE){}
+                //Toast.makeText(getApplicationContext(), getString(R.string.wearable_api_unavailable), Toast.LENGTH_LONG).show();
+            }
+        }).addApi(Wearable.API).build();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void makePhoneCall(int heartRate)
+    {
+        if (heartRate >= 50 ||  heartRate <= 30)
+        {
+            Log.i("Phone call", "heart rate is correct");
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:8016960277"));
+
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            startActivity(callIntent);
+            // texting
+            textContacts();
+
+            //notification
+            addNotification();
+        }
+    }
+
+    public void textContacts()
+    {
+        String phoneNumber = "8016960277";
+        String text = "Be Safe!";
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, text, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS failed, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void addNotification() {
+        Log.i("Start", "notification");
+
+   /* Invoking the default notification service */
+        NotificationCompat.Builder  mBuilder = new NotificationCompat.Builder(this);
+
+        mBuilder.setContentTitle("New Message");
+        mBuilder.setContentText("You've received new message.");
+        mBuilder.setTicker("New Message Alert!");
+        mBuilder.setSmallIcon(R.drawable.newhrt);
+
+   /* Increase notification number every time a new notification arrives */
+        mBuilder.setNumber(++numMessages);
+
+   /* Add Big View Specific Configuration */
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        String[] events = new String[5];
+        events[0] = new String("Medical condition:");
+        events[1] = new String("Allergies:");
+        events[2] = new String("current Medication:");
+        events[3] = new String("Blood Type:");
+        events[4] = new String("other: ");
+
+        // Sets a title for the Inbox style big view
+        inboxStyle.setBigContentTitle("Medication Condition:");
+
+        // Moves events into the big view
+        for (int i=0; i < events.length; i++) {
+            inboxStyle.addLine(events[i]);
+        }
+
+        mBuilder.setStyle(inboxStyle);
+
+        int notificationID = 990;
+
+   /* Creates an explicit intent for an Activity in your app */
+        Intent resultIntent = new Intent(this,MedConditionActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MedConditionActivity.class);
+
+   /* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+   /* notificationID allows you to update the notification later on. */
+        mNotificationManager.notify(notificationID, mBuilder.build());
+    }
+}
