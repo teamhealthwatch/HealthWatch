6cada9d4144249509babb59c928c39f09be5a4f0
taneyyy <taneyyy@gmail.com>
2017-12-07 18:07:22 -0700

    Wear: setup request and receive medication

---
+++ wear/src/main/java/com/example/android/healthwatch/Model/MedModel.java
@@ -0,0 +1,89 @@
+package com.example.android.healthwatch.Model;
+
+/**
+ * Created by faitholadele on 10/6/17.
+ */
+
+public class MedModel {
+
+    private int id;
+    String time;
+    String date;
+    String name;
+    String medMessage;
+    public String repeatDays;
+
+
+    public MedModel()
+    {
+
+    }
+
+    public MedModel(String name, String time, String date, String dosage, int id, String repeatDays){
+        this.time = time;
+        this.date = date;
+        this.name = name;
+        this.medMessage = dosage;
+        this.id = id;
+        this.repeatDays = repeatDays;
+    }
+
+    public MedModel(String time, String date, String dosage, int id, String repeatDays){
+        this.time = time;
+        this.date = date;
+        this.medMessage = dosage;
+        this.id = id;
+        this.repeatDays = repeatDays;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMedMessage() {
+        return medMessage;
+    }
+
+    public void setMedMessage(String medMessage) {
+        this.medMessage = medMessage;
+    }
+
+
+    public String getRepeatDays() {
+        return repeatDays;
+    }
+
+    public void setRepeatDays(String reapetDays) {
+        this.repeatDays = reapetDays;
+    }
+
+
+}
